336b6b5e7b5ab1eaae5c4acf48108572e677632d
NICOLA-200 <fatepepe66@gmail.com>
2026-03-22 18:37:12 +0200

    add jwt

--- backend/models/user.go
+++ backend/models/user.go
@@ -1,9 +1,17 @@
 package models
 
-import "gorm.io/gorm"
+import (
 
+"gorm.io/gorm"
+"golang.org/x/crypto/bcrypt"
+)
 type User struct {
 	gorm.Model          // adds ID, CreatedAt, UpdatedAt, DeletedAt
 	Username string `gorm:"size:50;unique;not null;index"`
 	Password string `gorm:"size:255;not null"` // will store bcrypt hash
+}
+
+func (u *User) CheckPassword(password string) bool {
+	err := bcrypt.CompareHashAndPassword([]byte(u.Password), []byte(password))
+	return err == nil
 }
